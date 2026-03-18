abb3dc59e20523141335ff0426901cabbe295dac
rahulshrivastav <rahulasclat@gmail.com>
2026-03-18 10:58:19 +0530

    Update App.jsx

--- web-admin/src/App.jsx
+++ web-admin/src/App.jsx
@@ -11,6 +11,8 @@ import ReportsPage from './pages/ReportsPage'
 import UsersPage from './pages/UsersPage'
 import SettingsPage from './pages/SettingsPage'
 import ProductDashboardPage from './pages/ProductDashboardPage'
+import EmailPage from './pages/EmailPage'
+import ActivityPage from './pages/ActivityPage'
 
 function PrivateRoute({ children }) {
   const { user } = useAuth()
@@ -31,14 +33,16 @@ export default function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<PrivateRoute><Layout /></PrivateRoute>}>
           <Route index element={<Navigate to="/dashboard" replace />} />
-          <Route path="dashboard" element={<DashboardPage />} />
-          <Route path="leads" element={<LeadsPage />} />
-          <Route path="followups" element={<FollowUpsPage />} />
-          <Route path="assign" element={<AdminRoute><AssignPage /></AdminRoute>} />
-          <Route path="reports" element={<ReportsPage />} />
-          <Route path="products" element={<ProductDashboardPage />} />
-          <Route path="users" element={<AdminRoute><UsersPage /></AdminRoute>} />
-          <Route path="settings" element={<AdminRoute><SettingsPage /></AdminRoute>} />
+          <Route path="dashboard"   element={<DashboardPage />} />
+          <Route path="leads"       element={<LeadsPage />} />
+          <Route path="followups"   element={<FollowUpsPage />} />
+          <Route path="assign"      element={<AdminRoute><AssignPage /></AdminRoute>} />
+          <Route path="reports"     element={<ReportsPage />} />
+          <Route path="products"    element={<ProductDashboardPage />} />
+          <Route path="users"       element={<AdminRoute><UsersPage /></AdminRoute>} />
+          <Route path="settings"    element={<AdminRoute><SettingsPage /></AdminRoute>} />
+          <Route path="email"       element={<EmailPage />} />
+          <Route path="activities"  element={<ActivityPage />} />
         </Route>
         <Route path="*" element={<Navigate to="/dashboard" replace />} />
       </Routes>
